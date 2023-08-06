43a46f5ad76e652e68bb651e098ac406b9af1832
Marlon Leoner <marlon.leoner@gmail.com>
2023-08-06 16:44:20 -0300

    Initial commit

---
+++ src/main/java/marlon/leoner/projectavaliacaotecnica/ProjectAvaliacaoTecnicaApplication.java
@@ -0,0 +1,13 @@
+package marlon.leoner.projectavaliacaotecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAvaliacaoTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAvaliacaoTecnicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marlon/leoner/projectavaliacaotecnica/ProjectAvaliacaoTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package marlon.leoner.projectavaliacaotecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAvaliacaoTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
